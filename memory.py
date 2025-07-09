8e9fd412bb777beaa9deef89a4df713cbc3ecbbe
VittorioGuida15 <115788185+VittorioGuida15@users.noreply.github.com>
2025-07-09 14:17:02 +0200

    Update memory.py

--- memory.py
+++ memory.py
@@ -2,7 +2,7 @@ import json
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes, CommandHandler
 
-TOKEN = "8135909690:AAGMgtdO6w71UyCewgSc3qnMxC_0RAqTdMU"
+TOKEN = "IL_TUO_TOKEN"
 FILE_PLAYERS = "players.json"
 
 # Carica i dati dal file JSON
